fbbf4839e8e55a15e798f77c235919cdfc357651
nado_15 <nadime@live.de>
2017-01-24 02:51:35 +0100

    Ignore Koch

---
+++ Pizza.java
@@ -0,0 +1,35 @@
+
+public class Pizza implements Koch {
+	private Koch koch;
+	
+	public static void main(String[] args) {
+		
+		GUI gui = new GUI();
+		Pizza ha = new Pizza();
+		Koch hallo = new Pizza();
+			
+	}
+	
+	public void ruf(){
+		int lol = Hallo(29);
+	}
+	@Override
+	public int Hallo(int haha) {
+		koch = new Pizza();
+		koch.drehen();
+		return 0;
+	}
+
+	@Override
+	public int zustand() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void drehen() {
+		System.out.println("wasgeht ab");
+		
+	}
+
+}
