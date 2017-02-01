cf8f3f878b455c2d6c7ee1805d8cb898e74a6414
k4ps <konradschoen@gmx.de>
2017-02-01 20:17:25 +0100

    Update Produkt.java

--- PizzaService/src/de/se/PizzaService/Produkt.java
+++ PizzaService/src/de/se/PizzaService/Produkt.java
@@ -9,6 +9,38 @@ package de.se.PizzaService;
  *
  * @author Malte
  */
-public class Produkt {
-    
+public class Produkt implements IProduktMitarbeiter {
+	private int id;
+	private String name;
+	private int preis;
+	private Rezept rezept;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPreis() {
+		return preis;
+	}
+
+	public void setPreis(int preis) {
+		this.preis = preis;
+	}
+
+	public Rezept getRezept() {
+		return rezept;
+	}
+
+	public void setRezept(Rezept rezept) {
+		this.rezept = rezept;
+	}
+
+	public int getId() {
+		return id;
+	}
+
 }
