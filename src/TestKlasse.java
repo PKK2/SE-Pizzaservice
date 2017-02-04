888ba5a7e24369277bac44297e840e650a3cdd0b
nado_15 <nadime@live.de>
2017-02-04 12:09:05 +0100

    TestKlasse, soll quasi die Bestellung sein...

---
+++ src/TestKlasse.java
@@ -0,0 +1,18 @@
+
+public class TestKlasse {
+	
+	private String name;
+	
+	public TestKlasse(String name) {
+		
+		this.name=name;
+	}
+	
+	public String toString (){
+		return name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
