630e8e042d23fef54c84bd0969c17dea4d8c81b5
nado_15 <nadime@live.de>
2017-02-05 21:21:21 +0100

    Add files via upload

--- src/GUI.java
+++ src/GUI.java
@@ -6,18 +6,19 @@ import java.io.*;
 @author Nadime B.
 */
 public class GUI {
-	JFrame kunde, koch;  	
-	JPanel kochPanel, kundePanel, kundePanelMitte;
-	ImageIcon pizzaSymbol;
-	Buttons buttons;
-	KochButtons kochbuttons;
-	public GUI () {
-		
+	private	JFrame kunde, koch;  	
+	private ImageIcon pizzaSymbol;
+	private Buttons buttons;
+	private KochButtons kochbuttons;
+	private IDataFactory datafactory;
+	
+	public GUI (IDataFactory datafactory) {
+		 this.datafactory = datafactory;
 		 kunde = new JFrame();
 		 koch = new JFrame();
-		 buttons = new Buttons(kunde);
+		 buttons = new Buttons(datafactory, kunde);
 		 kochbuttons = new KochButtons(koch);
-		 // TODO
+		
 		 pizzaSymbol = new ImageIcon("pizza.png");
 		 
 		 createKundeFrame();
@@ -39,7 +40,7 @@ public class GUI {
 		 koch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 koch.setIconImage(pizzaSymbol.getImage());
 		 koch.setLayout(new BorderLayout());
-	//	 koch.setResizable(false);
+		 koch.setResizable(false);
 		 koch.setLayout(null);
 		 PanelKoch();
 		
@@ -47,16 +48,11 @@ public class GUI {
 	
 	private void createKundeFrame() {
 		
-				 // Setze den Titel des Fensters
 				 kunde.setTitle("Kunde");
-				 // Setze die Groesse des Fensters
 				 kunde.setPreferredSize(new Dimension(670,600));
 				 kunde.setResizable(false);
 				 kunde.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				 kunde.getContentPane().setBackground(Color.RED);
-				 //System.out.println( new File("C:/Users/Nado/workspace/Pizzaservice/src/resources/hallo.jpg").exists());
-				 //Hier ist wichtig, natürlich den richtigen Pfad anzugeben. + dateiformat //am bestem my myclass suchen + dateiname :)
-				
 				 kunde.setIconImage(pizzaSymbol.getImage());
 				 kunde.setLayout(null);
 				 PanelKunde();
@@ -68,7 +64,6 @@ public class GUI {
 		JLabel ueberschrift = new JLabel("Pizza Service");
 		JLabel geschmack = new JLabel("Sorten:");
 		JLabel groesse = new JLabel("Groesse:");
-		JLabel adresse = new JLabel("Adresse:");
 		
 		ueberschrift.setFont(new Font(null,Font.BOLD,30));
 		ueberschrift.setBounds(230,10,200,30);
@@ -80,7 +75,7 @@ public class GUI {
 		kunde.add(geschmack);
 		kunde.add(groesse);
 		
-		//Kein guter Programmierstill, hätte man auch alles im Konstruktor von Buttons machen koennen....
+		//Kein guter Programmierstill, hätte man auch alles im Konstruktor von Buttons machen koennen.... siehe PanelKoch()
 		buttons.pizzaGeschmack();
 		buttons.addBestellButton();
 		buttons.pizzaSize();
@@ -88,12 +83,17 @@ public class GUI {
 		buttons.extraToppings();
 		buttons.addAbholButton();
 		buttons.addBestellungBeastigenButton();
+		buttons.addWeitereBestellung();
 	}
 	
 	
 	private void PanelKoch(){
 		kochbuttons.makeFrame();
 		
+	}
+	
+	public String toString() {
+		return "Gui fuer Kunde und Koch";
 	}
 	
 }
